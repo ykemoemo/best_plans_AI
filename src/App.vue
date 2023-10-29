c3eb07b8a61fa3afbe7e29173678aeb8b635bad4
ykemoemo <ykemoemo@gmail.com>
2023-10-30 01:03:49 +0900

    Add:2023/10/30

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,32 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <v-app>
+    <v-main>
+      <h1>Best plans AI</h1>
+      <WhatToEat/>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue'
+import WhatToEat from './components/WhatToEat.vue'
 
-@Options({
+export default defineComponent({
+
   components: {
-    HelloWorld,
+    WhatToEat,
+  },
+
+  data () {
+    return {
+      //
+    }
   },
 })
-export default class App extends Vue {}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  .v-main{
+    margin: 0 auto;
+  }
 </style>
