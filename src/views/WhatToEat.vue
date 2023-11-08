4669d7982a36d400d9856a681086b56c5f19d77d
ykemoemo <ykemoemo@gmail.com>
2023-11-09 00:00:35 +0900

    経緯、緯度の取得

--- src/views/WhatToEat.vue
+++ src/views/WhatToEat.vue
@@ -27,7 +27,7 @@
       <v-radio label="辛い" value="辛い"></v-radio>
       <v-radio label="なんでもいい" value="なんでもいい"></v-radio>
     </v-radio-group>
-    <p>場所は？: {{ Selected_5}}</p>
+    <p>場所は？: {{ Selected_5 }}</p>
     <v-text-field
       v-model="Selected_5"
       label="場所を入力"
@@ -35,14 +35,24 @@
       color="green darken-5"
       clearable
     ></v-text-field>
-    {{joined}}<br>
-    <v-btn v-btn v-on:click="join(Selected_1, Selected_2, Selected_3, Selected_4, Selected_5)">まとめる</v-btn>
+    <div>
+      <p>緯度:{{ latitude }}</p>
+      <p>経度:{{ longitude }}</p>
+      <p>住所:{{ address }}</p>
+      <v-btn v-btn v-on:click="getAddress()">現在地を取得</v-btn>
+    </div>
+    <div>
+      {{joined}}<br>
+      <v-btn v-btn v-on:click="join(Selected_1, Selected_2, Selected_3, Selected_4, Selected_5)">まとめる</v-btn>
+    </div>
   </v-container>
 </template>
 
 <script lang='ts' setup>
   import { ref } from "vue";
+  import axios from 'axios'
 
+  /* 入力値制御 */
   const Selected_1 = ref<string>("");
   const Selected_2 = ref<string>("");
   const Selected_3 = ref<string>("");
@@ -55,22 +65,32 @@
     return (joined);
   }
 
-  //data () {
-  //  return {
-  //    Selected_1: "" as string,
-  //    Selected_2: "" as string,
-  //    Selected_3: "" as string,
-  //    Selected_4: "" as string,
-  //    Selected_5: "" as string,
-  //    joined: "" as string
-  //  }
-  //},
-  //methods: {
-  //    join: function(s_1: string, s_2: string, s_3: string, s_4: string, s_5: string): string{
-  //        this.joined = s_1 + s_2 + s_3 + s_4 + s_5
-  //        return(this.joined)
-  //    }
-  //}
+  //位置情報取得処理
+  const latitude = ref<any>();
+  const longitude = ref<any>();
+  // ボタンを押した時の処理
+  const getAddress = () =>{
+    // Geolocation APIに対応している
+    if( navigator.geolocation ){
+      // 取得に成功した場合の処理
+      const successCallback = (position: any) => {
+        // 緯度を取得
+        latitude.value = position.coords.latitude;
+        // 経度を取得
+        longitude.value = position.coords.longitude;
+      }
+      // 取得に失敗した場合の処理
+      const errorCallback = () =>{
+        alert("位置情報が取得できませんでした");
+      }
+      // 位置情報を取得する
+      navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+      }
+    // Geolocation APIに対応していない場合
+    else{
+      // 現在位置を取得できない場合の処理
+      alert( "あなたの端末では、現在位置を取得できません。" ) ;
+    }
 </script>
 
 <style>
